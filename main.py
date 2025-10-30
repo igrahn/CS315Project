35eaa512f4a9392d5e541d074a101134c35c1c13
igrahn <162189356+igrahn@users.noreply.github.com>
2025-10-30 14:50:59 -0700

    Update main.py
    added initial libraries

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+#
+# Libraries to use, subject to change
+import argparse
+from src.io_ppm import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
+from src.filters import box_blur, sharpen, sobel_edges, otsu_threshold, apply_threshold
