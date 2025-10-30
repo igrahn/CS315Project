237b5e6c1f6d97aff494a00cf513708d5b03b41f
igrahn <162189356+igrahn@users.noreply.github.com>
2025-10-30 14:52:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #
 # Libraries to use, subject to change
 import argparse
-from src.io_ppm import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
+from src.ppm_io import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
 from src.filters import box_blur, sharpen, sobel_edges, otsu_threshold, apply_threshold
+
+def main();
