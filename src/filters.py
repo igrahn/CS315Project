73a9bf07e1d1a545a9e51b33acf4310a94037e46
igrahn <162189356+igrahn@users.noreply.github.com>
2025-10-30 14:56:17 -0700

    Update filters.py
    initial functions

--- src/filters.py
+++ src/filters.py
@@ -1,1 +1,16 @@
 
+def convolve_gray()
+
+def convolve_rgb()
+
+def box_blur()
+
+def sharpen(img)
+
+def sobel_edges(gray)
+
+def histogram(gray)
+
+def otsu_threshold(gray)
+
+def apply_threshold()
