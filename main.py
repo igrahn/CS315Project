47f7f7489440ee6e2248188aaad92c05ba5277c9
igrahn <162189356+igrahn@users.noreply.github.com>
2025-10-30 15:22:45 -0700

    Update main.py
    started working on main

--- main.py
+++ main.py
@@ -4,4 +4,22 @@ import argparse
 from src.ppm_io import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
 from src.filters import box_blur, sharpen, sobel_edges, otsu_threshold, apply_threshold
 
-def main();
+def main():
+
+  img = read_ppm(args.inp)
+
+  if args.op == "blur":
+    out = box_blur(img, k=args.k)
+  elif args.op == "sharpen":
+    out = sharpen(img)
+  elif args.op == "sobel":
+    #
+    #
+    #
+  elif args.op == "otsu":
+    #
+    #
+    #
+  else:
+    raise ValueError("error!")
+ 
