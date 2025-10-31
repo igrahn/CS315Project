16d61b359c4713155662149226600f35eb46a5f5
igrahn <162189356+igrahn@users.noreply.github.com>
2025-10-31 10:24:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ import argparse
 from src.ppm_io import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
 from src.filters import box_blur, sharpen, sobel_edges, otsu_threshold, apply_threshold
 
+def oarse_args():
+  p = argparse.ArgumentParser(description=""
+
 def main():
+
+    parser = argparse.ArgumentParser(description="Basic Image Processing Tool (blur, sharpen, sobel, otsu)")
+    parser.add_argument("--op", required=True)
+    parser.add_argument("--in", dest="inp", required=True)
+    parser.add_argument("--out", dest="outp", required=True)
+    parser.add_argument("--k", type=int, default=3)
+  
+    args = parser.parse_args()
 
   img = read_ppm(args.inp)
 
   if args.op == "blur":
     out = box_blur(img, k=args.k)
+    
   elif args.op == "sharpen":
     out = sharpen(img)
+    
   elif args.op == "sobel":
     #
     #
