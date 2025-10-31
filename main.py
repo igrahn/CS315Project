9f56614030a0eaa23d447dfe444e24d80fc574c2
igrahn <162189356+igrahn@users.noreply.github.com>
2025-10-31 13:13:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 from src.ppm_io import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
 from src.filters import box_blur, sharpen, sobel_edges, otsu_threshold, apply_threshold
 
-def oarse_args():
+def parse_args():
   p = argparse.ArgumentParser(description=""
 
 def main():
