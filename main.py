5950dd7e07d6f8a2acbc2a4ff8100d322bf2c5bc
igrahn <162189356+igrahn@users.noreply.github.com>
2025-11-13 23:42:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,15 @@
-#
-# Libraries to use, subject to change
-import argparse
-from src.ppm_io import read_ppm, write_ppm, rgb_to_gray, gray_to_rgb
-from src.filters import box_blur, sharpen, adjust_brightness, otsu_threshold, apply_threshold, invert
+#---------------------------------------------------------#
+# File: filters.py
+# Author: Ian Grahn and Matthew Barbarisi
+# Purpose: Used to be significantly more in depth, almost entirely scrapped in favor of interace.py
+# Version: 1.4 Nov 12 2025
+# Resources: Me and Matthew wroe the code, ChatGPT helped with compilation erros, and checking for
+# redundant or uselss parts of the code. It also helepd greatly with syntax and compilation errors.
+# OpenAI (2025) *ChatGPT* (Version 5) (Generative AI Model).
+# https://www.openai.com/chatgpt/
+#---------------------------------------------------------#
 
-def main():
-  parser = argparse.ArgumentParser(description="Basic Image Processing Tool (blur, sharpen, brightness, otsu, invert)")
-  
-  parser.add_argument("--op", required=True, choices=["blur", "sharpen", "brightness", "otsu", "invert"])
-  parser.add_argument("--in", dest="inp", required=True)
-  parser.add_argument("--out", dest="outp", required=True)
-  parser.add_argument("--k", type=int, default=3)
-  parser.add_argument("--delta", type=float, default=0.15)
-  
-  args = parser.parse_args()
+from interface import main
 
-  img = load_image(args.inp)
-
-  if args.op == "blur":
-    k = 5 if args.k == 5 else 3
-    out = box_blur(img, k=k)
-    
-  elif args.op == "sharpen":
-    out = sharpen(img)
-
-  elif args.op == "brightness":
-    out = adjust_brightness(img, delta=args.delta)
-    
-  elif args.op == "invert":
-    out = invert(img)
-    
-  elif args.op == "otsu":
-    
-    gray = rgb_to_gray(img)
-    t = otsu_threshold(gray)
-    binary = apply_threshold(gray, t)
-    out = gray_to_rgb(binary)
-    print(f"Otsu threshold value: {t:.3f}")
-    
-  else:
-    print("Invalid! Please choose from one of the 4 options.")
-
-
-  save_image(args.outp, out)
-  print(f"Saved output as {args.outp}")
-    
 if __name__ == "__main__":
     main()
- 
